f806c04e6b453842ab4edb755b363867d8f729da
aviaryan <avi.aryan123@gmail.com>
2016-05-22 20:59:44 +0530

    made main.js executable and run through electron, added bin in package.json

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # EmojiUI
 
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
 Simple Offline Emoji finding tool using Electron
 
 ## Install and Run
 
 ```
-git clone --recursive <project url>
+git clone --recursive https://github.com/aviaryan/emojiui.git
 npm install
 npm start
 ```
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env electron
+
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
@@ -71,6 +73,3 @@ app.on('activate', function () {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
